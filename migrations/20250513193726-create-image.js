ee6014a982221205b4ad764ed9d4530ceb7a15bd
Bhavya_J <bhavyajha42@gmail.com>
2025-05-16 14:38:10 +0530

    Added image upload functionality

---
+++ migrations/20250513193726-create-image.js
@@ -0,0 +1,42 @@
+// /migrations/20250513193726-create-image.js
+
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Images', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      filePath: {
+        type: Sequelize.STRING
+      },
+      location: {
+        type: Sequelize.STRING
+      },
+      timestamp: {
+        type: Sequelize.DATE
+      },
+      userId: {
+        type: Sequelize.INTEGER
+      },
+      status: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Images');
+  }
+};
